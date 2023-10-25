9d8154fe978873f8b67f15dad4c1af1c8cb45fa1
ahmadfadlie95 <ahmadfadlie95@gmail.com>
2023-10-25 16:27:11 +0800

    Added snackBar & alertDialog(Not Working)

--- lib/widgets/details.dart
+++ lib/widgets/details.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:to_dos/widgets/home.dart';
 
 class DetailsPage extends StatelessWidget {
-  // 1) Create a property/variable to store passed item
-  final Map<String,dynamic> todo;
-
+  // 1) Create a property/variable to store the passed item
+  final Map<String, dynamic> todo;
   final int index;
 
   // 2) Create the constructor of the class to receive the item
@@ -12,21 +12,51 @@ class DetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Details Page"),),
+      appBar: AppBar(
+        title: Text("Details Page"),
+      ),
       body: Center(
         child: Column(
           children: [
             Text(todo["name"]!),
             Text(todo["place"]!),
             Text(todo["desc"]!),
-            Row(mainAxisAlignment: MainAxisAlignment.center,
-              children: [ElevatedButton(onPressed: (){
-                Navigator.pop(context,{"index":index, "action":"mark"});
-              }, child: todo["completed"] ? Text("Unmark Completion"):Text("Mark as Completed")),
-                ElevatedButton(onPressed: (){
-                  // print("Delete ditekan untuk item index $index");
-                  Navigator.pop(context, {"index":index, "action":"del"});
-                }, child: Text("Delete Item"))],)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context, {"index": index, "action": "mark"});
+                  },
+                  child: todo["completed"]
+                      ? Text("Unmark Completion")
+                      : Text("Mark as Completed"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text("Delete Confirmation"),
+                          content: Text("Are you sure you want to delete this?"),
+                          actions: [
+                            TextButton(
+                              child: Text("Delete"),
+                              onPressed: () {
+                                Navigator.pop(context, {"action":"del"});
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Text("Delete Item"),
+                ),
+              ],
+            ),
           ],
         ),
       ),
